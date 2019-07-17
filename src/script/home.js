bc41595a79fa7a7e2a9364df6c43312a05a95afe
Daniel Fazlijevic <danielfazlijevic98@gmail.com>
2019-07-17 22:19:15 +0200

    ref: glide u home

--- src/script/home.js
+++ src/script/home.js
@@ -2,26 +2,32 @@ import Glide from '@glidejs/glide';
 
 import './granim';
 
-
-const glide = new Glide('.glide').mount()
-
 const sliderButtons = document.querySelectorAll('.slider-button');
+
+const glide = new Glide('.glide')
+
+
+glide.on('run.before', () => {
+    sliderButtons[glide.index].classList.remove('active');
+
+})
+
+glide.on('run', () => {
+    sliderButtons[glide.index].classList.add('active');
+})
+
+glide.mount();
+
 sliderButtons[0].classList.add('active');
 
 for (let i = 0; i < sliderButtons.length; i++) {
     sliderButtons[i].addEventListener('click', (e) => {
         e.preventDefault();
-        slideTo(i);
+        glide.go(`=${i}`);
     });
 }
 
-function slideTo(index) {
-    if (document.querySelector('.slider-button.active'))
-        document.querySelector('.slider-button.active').classList.remove('active');
-    sliderButtons[index].classList.add('active');
-    // console.log(i);
-    glide.go(`=${index}`);
-}
+
 
 const glideTestimonials = new Glide('.glider-testimonials', {
     autoplay: 3000,
