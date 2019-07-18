57bc5cadc451a22ef6965077c6d7bc37f7f36152
Daniel Fazlijevic <danielfazlijevic98@gmail.com>
2019-07-18 19:06:34 +0200

    kontakt forma

--- src/script/contact.js
+++ src/script/contact.js
@@ -1,1 +1,48 @@
 import './maps';
+
+import { debug } from './utilities';
+
+const form = document.querySelector('.form-contact');
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    debug('form submit');
+
+    const name = document.querySelector('#name').value;
+    const email = document.querySelector('#email').value;
+    const message = document.querySelector('#message').value;
+
+    const data = {
+        service_id: 'default_service',
+        template_id: '4bees_contact',
+        user_id: 'user_gBC0OFgBaTeHpptnyebd4',
+        template_params: {
+            name,
+            email,
+            message
+        }
+    }
+    const allData = JSON.stringify(data)
+    const url = "https://api.emailjs.com/api/v1.0/email/send"
+
+    fetch(url, {
+            method: 'POST', // or 'PUT'
+            body: allData, // data can be `string` or {object}!
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => {
+            alert('Message sent!');
+            document.querySelector('#name').value = "";
+            document.querySelector('#email').value = "";
+            document.querySelector('#message').value = "";
+            debug('Success:', response)
+        })
+        .catch(error => {
+            alert("There was an error in sending your message. Please try again later.")
+            console.error('Error:', error)
+        });
+
+});
