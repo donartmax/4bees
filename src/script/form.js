83fd1fa1b4105bbd7344882d5212ebbdec775e26
Daniel Fazlijevic <danielfazlijevic98@gmail.com>
2019-07-19 14:15:42 +0200

    infopack slanje na mejl

--- src/script/form.js
+++ src/script/form.js
@@ -14,6 +14,33 @@ if(form){
             form.classList.add('sucess');
             form.querySelector('button').innerText = "Great! Thank you."
             submited = true;
+            const data = {
+                service_id: 'default_service',
+                template_id: '4bees_infopack',
+                user_id: 'user_gBC0OFgBaTeHpptnyebd4',
+                template_params: {
+                    email
+                }
+            }
+            const allData = JSON.stringify(data)
+            const url = "https://api.emailjs.com/api/v1.0/email/send"
+        
+            fetch(url, {
+                    method: 'POST', // or 'PUT'
+                    body: allData, // data can be `string` or {object}!
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                })
+                .then(response => {
+                    alert('You will get an email shortly.');
+                    debug('Success:', response)
+                })
+                .catch(error => {
+                    alert("There was an error in submitting your email. Please try again later.")
+                    console.error('Error:', error)
+                });
+        
         }
         else{
             form.classList.add('error');
