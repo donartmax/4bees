f2ec1b2cd31f9005dc37e4038d2a5d44232707d7
Marijana Maksimovic <maxmarijana@live.com>
2019-08-04 11:30:37 +0200

    ikonice za phase i tech logoi

--- src/script/howwedoit.js
+++ src/script/howwedoit.js
@@ -2,18 +2,18 @@ import Glide from '@glidejs/glide';
 
 
 const glideTech = new Glide('.glide-technologies', {
-    autoplay: 2500,
-    type: 'carousel',
-    perView: 4,
-    focusAt: 'center',
-    breakpoints: {
-      800: {
-        perView: 2
-      },
-      480: {
-        perView: 1
-      }
+  autoplay: 2500,
+  type: 'carousel',
+  perView: 4,
+  focusAt: 'center',
+  breakpoints: {
+    800: {
+      perView: 2
+    },
+    480: {
+      perView: 1
     }
-  })
-  
-  glideTech.mount()
+  }
+})
+
+glideTech.mount()
