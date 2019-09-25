2ff7b9fa14665c104e254f9c3c8e2a928dd18640
Daniel Fazlijevic <danielfazlijevic98@gmail.com>
2019-09-26 00:28:53 +0200

    validacija za get estimation

--- src/script/get_estimation.js
+++ src/script/get_estimation.js
@@ -10,15 +10,32 @@ const partsCount = estimationParts.length;
 
 debug(estimationParts);
 
+
 estimationBtn.addEventListener('click', (e) => {
     e.preventDefault();
     let types = [];
     let scopes = [];
+    debug(activeIndex);
+    
+    let name = document.querySelector('#name').value;
+    let email = document.querySelector('#email').value;
+    let phoneNumber = document.querySelector('#phone-number').value;
+    let description = document.querySelector('#description').value;
+    
+    // form input
+    if(activeIndex === 2){
+        if(name.trim().length === 0 || email.trim().length === 0 || description.trim().length === 0){
+            debug('NIJE POPUNJENO NESTO');
+            document.querySelector('.alert-danger').classList.remove('d-none')
+            return;
+        }
+    }
     activeIndex++;
 
     if (activeIndex < partsCount)
         setActive(activeIndex);
 
+    
     if (activeIndex == partsCount - 1) {
         estimationBtn.style.display = 'none';
         debug('Finished form');
@@ -26,11 +43,11 @@ estimationBtn.addEventListener('click', (e) => {
         const types = getSelected(1)
         const scopes = getSelected(2)
 
-        // TODO dodati slanje forme
-        const name = document.querySelector('#name').value;
-        const email = document.querySelector('#email').value;
-        const phoneNumber = document.querySelector('#phone-number').value;
-        const description = document.querySelector('#description').value;
+       
+        name = document.querySelector('#name').value;
+        email = document.querySelector('#email').value;
+        phoneNumber = document.querySelector('#phone-number').value;
+        description = document.querySelector('#description').value;
 
         const data = {
             service_id: 'default_service',
@@ -47,7 +64,9 @@ estimationBtn.addEventListener('click', (e) => {
         }
         const allData = JSON.stringify(data)
         const url = "https://api.emailjs.com/api/v1.0/email/send"
-
+        console.log(allData);
+       
+        return;
         fetch(url, {
                 method: 'POST', // or 'PUT'
                 body: allData, // data can be `string` or {object}!
